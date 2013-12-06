9bfe598ccb0982536afca7624eb61958a4864322
Arnelle Balane <arnellebalane@gmail.com>
2013-12-07 01:39:04 +0800

    xhttp library to be able to call sms sending endpoint

--- application/models/sms_model.php
+++ application/models/sms_model.php
@@ -12,11 +12,25 @@
     }
 
     public function send($recepient, $content) {
-      echo '<pre>';
-      print_r($recepient);
-      echo '<br/>';
-      print_r($content);
-      echo '</pre>';
+      // echo '<pre>';
+      // print_r($recepient);
+      // echo '<br/>';
+      // print_r($content);
+      // echo '</pre>';
+
+      $this->load->library('xhttp');
+
+      $data['post'] = array(
+        'uName' => 'u1vhn7dkm',
+        'uPin' => '21737086',
+        'MSISDN' => $recepient,
+        'messageString' => $content,
+        'Display' => '1',
+        'udh' => '',
+        'mwi' => '',
+        'coding' => '0'
+      );
+      xhttp::fetch('http://iplaypen.globelabs.com.ph:1881/axis2/services/Platform/sendSMS', $data);
     }
 
   }
